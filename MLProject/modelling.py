529ff108cafd9f7d0a7265f3bf4e50c875ee0887
chessyyriaa <chessyyriaa@gmail.com>
2025-12-23 17:07:12 +0700

    Update modelling.py

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -9,7 +9,6 @@ import os
 mlflow.autolog()
 
 def load_data(filename):
-
     base_dir = os.path.dirname(os.path.abspath(__file__))
     full_path = os.path.join(base_dir, filename)
     if not os.path.exists(full_path):
@@ -19,7 +18,8 @@ def load_data(filename):
 def main():
 
     mlflow.set_tracking_uri("file:./mlruns")
-    mlflow.set_experiment("CI_CD_Experiment")
+    
+
 
     print("Loading data...")
     df = load_data("churn_data_clean.csv")
@@ -35,17 +35,20 @@ def main():
     model = RandomForestClassifier(n_estimators=100, max_depth=10, random_state=42)
     model.fit(X_train, y_train) 
 
-
     y_pred = model.predict(X_test)
     acc = accuracy_score(y_test, y_pred)
     print(f"Final Accuracy: {acc}")
 
 
-    run = mlflow.last_active_run()
-    run_id = run.info.run_id
-    print(f"Run ID from Autolog: {run_id}")
+    run = mlflow.active_run()
     
+    if run is None:
 
+        run = mlflow.last_active_run()
+        
+    run_id = run.info.run_id
+    print(f"Run ID detected: {run_id}")
+    
     with open("run_id.txt", "w") as f:
         f.write(run_id)
 
